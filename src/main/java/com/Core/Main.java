76b8e1dc4616107a342c20b0296a84bab92be823
Arkadiusz Szymanski <arkadiuszsz@gmail.com>
2020-02-05 22:47:31 +0100

    Initial commit

---
+++ src/main/java/com/Core/Main.java
@@ -0,0 +1,15 @@
+package com.Core;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(Main.class);
+        logger.info("Hello World");
+
+}
+
+}
---
+++ src/main/java/com/Core/Servlett.java
@@ -0,0 +1,5 @@
+package com.Core;
+
+public class Servlett {
+    
+}
