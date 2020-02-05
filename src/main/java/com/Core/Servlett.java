4ad997ebd9dc61003d7df48917382b02ad338ca7
Arkadiusz Szymanski <arkadiuszsz@gmail.com>
2020-02-05 23:00:54 +0100

    INIT

--- src/main/java/com/Core/Servlett.java
+++ src/main/java/com/Core/Servlett.java
@@ -1,5 +1,14 @@
 package com.Core;
 
-public class Servlett {
-    
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class Servlett extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
 }
