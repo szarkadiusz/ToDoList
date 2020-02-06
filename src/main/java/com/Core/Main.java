fd0f943c6f7d23797be7cfa7fc632c94725644f9
Arkadiusz Szymanski <arkadiuszsz@gmail.com>
2020-02-06 13:36:54 +0100

    added class serviss

--- src/main/java/com/Core/Main.java
+++ src/main/java/com/Core/Main.java
@@ -1,15 +1,45 @@
 package com.Core;
 
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.eclipse.jetty.annotations.AnnotationConfiguration;
+import org.eclipse.jetty.plus.webapp.EnvConfiguration;
+import org.eclipse.jetty.plus.webapp.PlusConfiguration;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.*;
+
+import javax.xml.namespace.QName;
+
 
 public class Main {
 
-    public static void main(String[] args) {
-        Logger logger = LoggerFactory.getLogger(Main.class);
-        logger.info("Hello World");
+    public static void main(String[] args) throws Exception {
 
-}
+        var webApp = new WebAppContext();
+        webApp.setResourceBase("src/main/webapp");
+        webApp.setContextPath("/");
+
+        webApp.setConfigurations(new Configuration[]
+                {
+                        new AnnotationConfiguration(),
+                        new WebInfConfiguration(),
+                        new WebXmlConfiguration(),
+                        new MetaInfConfiguration(),
+                        new FragmentConfiguration(),
+                        new EnvConfiguration(),
+                        new PlusConfiguration(),
+                        new JettyWebXmlConfiguration()
+                });
+        webApp.setAttribute("org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern", ".*/classes/.*");
+//        webApp.addServlet(Servlett.class,"/api/*");
+
+
+        var server = new Server(8080);
+        server.setHandler(webApp);
+
+        server.start();
+        server.join();
+
+
+    }
 
 }
--- src/main/java/com/Core/Servlett.java
+++ src/main/java/com/Core/Servlett.java
@@ -1,14 +1,28 @@
 package com.Core;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Optional;
 
+
+@WebServlet (name = "Hello", urlPatterns = {"/api/*"})
 public class Servlett extends HttpServlet {
+    private final Logger logger= LoggerFactory.getLogger(Servlett.class);
+
+private static final String NAME= "name";
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+//        super.doGet(req, resp);
+        ;
+        logger.info("Request"+req.getParameterMap()+" got");
+        var parameter = Optional.ofNullable(req.getParameter(NAME)).orElse("world");
+        resp.getWriter().write("Elo" + parameter + "!");
     }
 }
